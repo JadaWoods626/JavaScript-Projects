92aefc54f955fe02abca175ef74fe7c64d3a8e97
= <122753731+JadaWoods626@users.noreply.github.com>
2023-02-09 15:10:45 -0600

    assignment 69

--- Basic java projects/js/main.js
+++ Basic java projects/js/main.js
@@ -2,9 +2,9 @@ var A = "this is the beginning of a string"
 var B = " and this is the end of the string" 
 var C = " (3+3)"
 document.write(A+B+C)
-
+// the function will display when you click "click here"//
 function myFunction() {
   var sentence = "I am Learning";
-        sentence += " a lot from this book";
-    document.getElementById("Concatenate").innerHTML = sentence;
+  sentence += " a lot from this book";
+  document.getElementById("Concatenate").innerHTML = sentence;
 }
